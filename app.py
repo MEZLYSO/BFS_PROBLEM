d4291d1e816d61fc2c56be6c560825eddf61d66b
MEZLY <lynuxazura@gmail.com>
2026-03-19 08:14:37 -0600

    .

--- app.py
+++ app.py
@@ -73,5 +73,4 @@ def calcular():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
