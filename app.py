8aa58531db6b444703e8eee51b14769334520969
MEZLY <lynuxazura@gmail.com>
2026-03-19 08:07:24 -0600

    V1

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 
 from arbol import Nodo
 from flask import Flask, request
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 
 def buscar_solucion(conexiones, estado_inicial, solucion):
@@ -70,4 +73,5 @@ def calcular():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
